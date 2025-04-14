c609c597411ffc4ab78a4ae4c78c3eb5d39722d0
Bahriddin <bahridnurullav@gmail.com>
2025-04-14 15:28:33 +0500

    ssssss

--- src/dialogs/import-dbml-dialog/import-dbml-dialog.tsx
+++ src/dialogs/import-dbml-dialog/import-dbml-dialog.tsx
@@ -87,35 +87,34 @@ export const ImportDBMLDialog: React.FC<ImportDBMLDialogProps> = ({
     const envId = searchParams.get('environment_id');
 
     const fetDbmlFile = async (): Promise<void> => {
-        try {
-            const response = await fetch(
-                `https://admin-api.ucode.run/v1/chart?project-id=${projectId}&environment-id=${envId}`
-            );
+        console.log('projectId', projectId, envId, setContent);
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            const resData: {
-                data?: {
-                    dbml?: string;
-                };
-            } = await response.json();
-
-            const dbml = resData?.data?.dbml;
-
-            if (dbml) {
-                setContent(dbml);
-                setDBMLContent(dbml);
-            }
-        } catch (error) {
-            console.error('Failed to fetch DBML file:', error);
-        }
+        // try {
+        //     const response = await fetch(
+        //         `https://admin-api.ucode.run/v1/chart?project-id=${projectId}&environment-id=${envId}`
+        //     );
+        //     if (!response.ok) {
+        //         throw new Error(`HTTP error! Status: ${response.status}`);
+        //     }
+        //     const resData: {
+        //         data?: {
+        //             dbml?: string;
+        //         };
+        //     } = await response.json();
+        //     const dbml = resData?.data?.dbml;
+        //     if (dbml) {
+        //         setContent(dbml);
+        //         setDBMLContent(dbml);
+        //     }
+        // } catch (error) {
+        //     console.error('Failed to fetch DBML file:', error);
+        // }
     };
 
     useEffect(() => {
         fetDbmlFile();
     }, []);
+
     const initialDBML = content ?? '';
 
     const [dbmlContent, setDBMLContent] = useState<string>(initialDBML);
